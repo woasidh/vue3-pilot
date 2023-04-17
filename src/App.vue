4d5d0a3ce863dbf4f4e84a260529c31cc129518b
최민우 <woasidh1@naver.com>
2023-04-17 21:22:33 +0900

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <button @click="changePerson">change person!</button>
+  <span>{{ person.name }}</span>
+  <span>{{ person.age }}</span>
+  <br/>
+  <button @click="changerPet">change person!</button>
+  <span>{{ pet.name }}</span>
+  <span>{{ pet.age }}</span>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {onMounted, ref} from "vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const person = ref({name: 'choi', age: 28});
+
+    const changePerson = () => {
+      person.value = {
+        name: 'kim',
+        age: 30,
+      }
+    }
+
+    onMounted(() => {
+      person.value.name += '(mounted)'
+    })
+
+    const pet = ref({type: 'dog', name: 'wondoo'});
+
+    const changerPet = () => {
+      pet.value = {
+        type: 'cat',
+        name: 'woori',
+      }
+    }
+
+    onMounted(() => {
+      pet.value.name += '(mounted)'
+    })
+
+    return { person, changePerson, pet, changerPet }
   }
+
 }
 </script>
 
